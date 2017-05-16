0b8df2b447c00a3df9365b9dfeb17d87d19f1b59
dragonzurfer <dragonzurfer@gmail.com>
2017-05-16 18:26:36 +0530

    SPOJ classical:DQUERY

--- dquery.cpp
+++ dquery.cpp
@@ -11,18 +11,23 @@ using namespace std;
 #define endl '\n'
 #define clr(a) memset(a,0,sizeof(a))
 #define all(x) x.begin(),x.end()
+#ifndef ONLINE_JUDGE
+#define gc getchar
+#define pc putchar
+#else
+#define gc getchar_unlocked
+#define pc putchar_unlocked
+#endif
 typedef int ll;
 
 struct query
 {
-	ll r,l,fa;
-	int ans;
+	ll r,l,ans;
+	int index;
 };
-
-ll cnt[100000],a[100000];
 int rootn, answ;
 
-bool block(query a,query b)
+inline bool block(query a,query b)
 {
     
   if(floor(a.l/rootn)<floor(b.l/rootn))
@@ -32,86 +37,106 @@ bool block(query a,query b)
   return a.r<b.r;
 }
 
-bool cmp(query a,query b)
+inline bool cmp(query a,query b)
 {
-  return a.ans<b.ans;
+  return a.index<b.index;
 }
 
-void insert(int pos)
-{
-//	cout<<endl<<"add "<<a[pos];
-	cnt[a[pos]]++;
- 	if(cnt[a[pos]]==1)
-		answ++;
-	
+int read_int() {
+    char c = gc();
+    while((c < '0' || c > '9') && c != '-') c = gc();
+    int ret = 0, neg = 0;
+    if (c == '-') neg = 1, c = gc();
+    while(c >= '0' && c <= '9') {
+        ret = 10 * ret + c - 48;
+        c = gc();
+    }
+    return neg ? -ret : ret;
 }
-void remove(int pos)
-{
-//	cout<<endl<<"rem "<<a[pos];
-	cnt[a[pos]]--;
-	if(cnt[a[pos]] == 0)
-		answ--;
+
+ll read_ll() {
+    char c = gc();
+    while((c < '0' || c > '9') && c != '-') c = gc();
+    ll ret = 0;
+    int neg = 0;
+    if (c == '-') neg = 1, c = gc();
+    while(c >= '0' && c <= '9') {
+        ret = 10 * ret + c - 48;
+        c = gc();
+    }
+    return neg ? -ret : ret;
 }
 
 int main()
 { 
-	ios::sync_with_stdio(false);
 	ll i,n,t,q,querylen;
-	cin>>n;
+	n=read_ll();
 	answ=0;	
-	clr(a);
+   vll a(n+1);
+   vll cnt(1000005);
 	FOR1(i,n-1)
-	cin>>a[i];
-	cin>>querylen;
-	ll qcnt=0;
-	while(qcnt!=querylen)
-	{
-		
-	answ=0;
-	if(querylen<100000)
-		q=querylen;
-	else
-		q=100000;
+	a[i]=read_ll();
+	q=read_ll();
 
-	query qa[q];
+	vector<query> qa(q);
 
 	for(i=0;i<q;i++)
    	{	
    		ll l,rr;
- 		cin>>l>>rr;
- 		qcnt+=1;
+ 		l=read_ll(),rr=read_ll();
 		qa[i].l = l-1;
 		qa[i].r = rr - 1;
-		qa[i].ans = i;
+		qa[i].index = i;
  	}	
+ 	vll answer(q,0);
+	rootn=int(sqrt(n))+1;
 
-	clr(cnt);
-	rootn=pow(n,0.5);
-
-	sort(qa,qa+q,block);
+	sort(all(qa),block);
 	ll currentL=0,currentR=0;
   
-	
+	ll pos;
 	for(i=0;i<q;i++)
   	{
-  		// int t;cin>>t;
       		while(currentL<qa[i].l)
-          	{	remove(currentL);currentL++; }
+          	{	
+          		pos=currentL;
+          		cnt[a[pos]]--;
+          			if(cnt[a[pos]] == 0)
+          				answ--;
+          		currentL++; 
+          	}
       		while(qa[i].l<currentL)
-          	{	insert(currentL-1);currentL--; }
+          	{	
+          		pos=currentL-1;
+      			cnt[a[pos]]++;
+      		 	if(cnt[a[pos]]==1)
+      				answ++;
+          		currentL--; 
+          	}
       		while(currentR<=qa[i].r)
-           	{	insert(currentR);currentR++;}
+           	{	
+           		pos=currentR;
+           		cnt[a[pos]]++;
+           		 	if(cnt[a[pos]]==1)
+           				answ++;
+           		currentR++;
+           	}
       		while(currentR>qa[i].r+1)
-          	{	remove(currentR-1);
-			currentR--;
-		}
+          	{	
+          		pos=currentR-1;
+          		cnt[a[pos]]--;
+          			if(cnt[a[pos]] == 0)
+          				answ--;
+          		currentR--;
+          	}
 
-      		qa[qa[i].ans].fa=answ;
+		qa[i].ans=answ;
   	}
-  	
+
+  	sort(all(qa),cmp);
 	for(ll i=0;i<q;i++)
-		cout<<qa[i].fa<<endl;
-	}
+		cout<<qa[i].ans<<endl;
+
 	return 0;
 }
 
